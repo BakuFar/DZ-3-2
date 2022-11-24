e95b44159636df11d5ff25af0cc2f6a2f98964cd
Farhat <baku.far@gmail.com>
2022-11-24 16:48:19 +0600

    Initial Commit

---
+++ DZ#3-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DZ#3-2
+//
+//  Created by Фархат Сталбек уулу on 24/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
